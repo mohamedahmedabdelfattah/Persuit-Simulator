baf4235d4b9540ae7143ffce0beb9e024c60a6f8
Mohamed Ahmed Abdel Fattah <mohamedahmedabdelfattah@gmail.com>
2015-02-24 17:40:55 +0200

    Initial Commit 2

---
+++ Persuit Simulator/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Persuit Simulator")]
+[assembly: AssemblyDescription("Modeling and Simulation Persuit Study Case Simulator")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("DarkOS")]
+[assembly: AssemblyProduct("Persuit Simulator")]
+[assembly: AssemblyCopyright("Copyright © DarkOS 2011")]
+[assembly: AssemblyTrademark("By Mohamed Ahmed Abdel Fattah")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("57335b2b-c6d0-4e43-b7e3-8ea333f4d5ed")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
